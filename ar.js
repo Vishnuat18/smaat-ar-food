2194bc62565b853483aff8ec9878a36bfeeafef3
Vishnu <vishnurajan24766@gmail.com>
2026-04-04 00:36:24 +0530

    v3

--- ar.js
+++ ar.js
@@ -15,20 +15,17 @@ async function startAR() {
 
   const { renderer, scene, camera } = mindarThree;
 
-  // Light
   const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
   scene.add(light);
 
-  // Anchor
   const anchor = mindarThree.addAnchor(0);
 
-  // Load model
   const loader = new THREE.GLTFLoader();
 
   loader.load(models[selectedDish], (gltf) => {
     const model = gltf.scene;
 
-    model.scale.set(0.6, 0.6, 0.6);
+    model.scale.set(0.5, 0.5, 0.5);
     model.position.set(0, 0, 0);
 
     anchor.group.add(model);
